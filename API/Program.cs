f18c8a5babb24e1f738842e5faf85fa7d32db3db
GrexThinh <vanthinhnq@gmail.com>
2023-11-10 17:01:28 +0700

    end of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,20 @@
 using API.Data;
 using API.Middleware;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-
 // Add services to the container.
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt=> { 
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
 builder.Services.AddCors();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -28,13 +23,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(opt => 
 {
-    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://127.0.0.1:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
@@ -45,7 +38,6 @@ try
 }
 catch (Exception ex)
 {
-    logger.LogError(ex, "A problem occured during migration");
+    logger.LogError(ex, "A problem occurred during migration");
 }
-
 app.Run();
